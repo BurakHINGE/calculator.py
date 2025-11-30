903684283adeb30ce06a7a76b322a1900f835fb9
BurakHINGE <burakmentese16@gmail.com>
2025-11-30 16:40:14 +0300

    Update eng_calculator.py
    Üssünü alma eklendi.
    Take power of number has been added to code.

--- eng_calculator.py
+++ eng_calculator.py
@@ -6,23 +6,32 @@ class Operation:
         self.operator = operator
 
     def calculate(self):
-        if self.operator not in "+-*/:":
-            return "Please select a valid operator (+, -, *, /, :): "
+
+        valid_ops = ["+", "-", "*", "/", ":", "**"]
+
+        if self.operator not in valid_ops:
+            return "Please select a valid operator (+, -, *, /, :, **):"
 
         if self.operator == "+":
-            return self.num1 + self.num2 
+            return self.num1 + self.num2
+        
         elif self.operator == "-":
             return self.num1 - self.num2
+        
         elif self.operator == "*":
             return self.num1 * self.num2
+        
         elif self.operator in ["/", ":"]:
             return self.num1 / self.num2
-      
+
+        elif self.operator == "**":
+            return self.num1 ** self.num2
+
 
 while True:
     num1 = int(input("Enter your first number: "))
-    operator = input("Choose your operator: ")
+    operator = input("Choose an operator: ")
     num2 = int(input("Enter your second number: "))
 
-    operation1 = Operation(num1, num2, operator)
-    print("Result:", operation1.calculate())
+    operation = Operation(num1, num2, operator)
+    print("Result:", operation.calculate())
